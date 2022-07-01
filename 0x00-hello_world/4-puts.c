17609e06bf6bea28ab5664fb8cba32fd9bd12444
Priceless-P <prisca.priceless11@gmail.com>
2022-07-01 14:05:01 -0700

    Add mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * *main- Entry point
+ * 
+ * Return: 0 (at success)
+ */
+int main(void)
+{
+	puts("\n"Programming is like building a multilingual puzzle");
+	return (0);
+}
