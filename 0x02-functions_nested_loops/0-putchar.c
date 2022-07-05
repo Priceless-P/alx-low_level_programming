869c12182c09bf330800179356ba9983d5c3a0f9
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 08:02:49 -0700

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Prisca Chidimma Maduka
+ */
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char stmt* = "_putchar";
+
+	putchar("%s \n", stmt);
+
+	return (0);
+}
