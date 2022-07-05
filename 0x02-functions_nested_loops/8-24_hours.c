ebb87e5d8da763d8c7837e56cd5bafacdcfb177d
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 15:42:30 -0700

    Hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,13 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (min= 0; min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
-			_putchar(hour + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar(min + '0');
+			_putchar((min % 10) + '0');
+			_putchar((min % 10) + '0');
 			min++;
 		}
 	}
