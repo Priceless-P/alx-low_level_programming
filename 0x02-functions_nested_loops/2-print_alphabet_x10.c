75f322d114a3521a3605f2299419220706e2e7e2
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 11:38:37 -0700

    Hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 	int i;
 	char j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-			_putchar(i);
+			_putchar(j);
 		_putchar('\n');
 	}
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- *
  * print_sign - Check desciption
  * Description: This function that prints the sign of a number
- *
+ * @c: Input value
  * Return: 1 and prints + if n is greater than zero
  * Return: 0 and prints 0 if n is zero
  * Return: -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
-	if (n<0)
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
-	}else if (n==0)
+	} else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	}else {
+	} else
+	{
 		_putchar('+');
 		return (1);
 	}
