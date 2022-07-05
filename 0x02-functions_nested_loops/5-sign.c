aaff5175c1a164cf47a31947672049f6a518dac9
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 11:45:57 -0700

    Betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,7 @@
  * print_sign - Check desciption
  * Description: This function that prints the sign of a number
  * @c: Input value
- * Return: 1 and prints + if n is greater than zero
- * Return: 0 and prints 0 if n is zero
- * Return: -1 and prints - if n is less than zero
+ * Return: 1 if n is greater than zero,0 if n is zero, -1 if n is less than zero
  */
 int print_sign(int n)
 {
@@ -18,9 +16,7 @@ int print_sign(int n)
 	{
 		_putchar('0');
 		return (0);
-	} else
-	{
-		_putchar('+');
-		return (1);
 	}
+	_putchar('+');
+		return (1);
 }
