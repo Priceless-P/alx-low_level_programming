8969f856239d5d70b6bbc117631248955d88e694
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 15:17:12 -0700

    Addtask 8 and 9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Description: prints every minute of the day of starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int min = 0;
+
+	while (hour <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar(hour + '0');
+			_putchar(':');
+			_putchar(min + '0');
+			min++
+		}
+		hour++;
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Description: THis function prints the 9 times table,starting with 0
+ */
+
+void times_table(void)
+{
+	int a = 9;
+	int b = 0;
+	int p;
+
+	while (b <= 9)
+	{
+		p = a * b;
+		_putchar(p);
+		_putchar(','0);
+		_putchar(' ');
+		_putchar('\n');
+		b++
+	}
+}
