8009c1ff9db4a3efebaf80cdb6e1a4e7fc85da4a
Priceless-P <prisca.priceless11@gmail.com>
2022-07-06 16:02:04 -0700

    Betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Check Description
- * Description: test that the function positive_or_negative() gives the correct output when given a case of 0
+ * Description:test that positive_or_negative give correct output for case of 0
  * Return: 0 on success
  */
 
 int main(void)
 {
 	int a;
-	
+
 	a = 0;
 	positive_or_negative(a);
 
