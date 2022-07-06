d2720cc35faa187c1304f324a1d1119ae3ff079b
Priceless-P <prisca.priceless11@gmail.com>
2022-07-06 15:08:17 -0700

    Add task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Check Description
+ * Description:  test that the function positive_or_negative() gives the correct output when given a case of 0
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int a;
+	
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
