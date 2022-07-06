13545a47a3f814df3993284fdf6288db40e900f0
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 18:26:26 -0700

    Hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,12 +24,14 @@ void print_to_98(int n)
 	}
 	else
 	{
-
-		for (i = n; i <= 98 ; i++)							{											if (i != n)
+		for (i = n; i <= 98 ; i++)
+		{
+			if (i != n)
 			{
 				printf(", ");
 			}
-			printf("%d", i);							}
+		printf("%d", i);
+		}
 	}
 	putchar('\n');
 
