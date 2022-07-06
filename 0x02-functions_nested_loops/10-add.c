c66aa6b91a56d5c66e656a5bf878a0034b59e54a
Priceless-P <prisca.priceless11@gmail.com>
2022-07-05 17:16:50 -0700

    Betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,6 +5,7 @@
  * Description: This function  adds two integers and returns the result
  * @a: One of the number to be added
  * @b: The other number to be added
+ * Return: sum of the two numbers
  */
 
 int add(int a, int b)
