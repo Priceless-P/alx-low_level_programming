a5583030949bdb3d25dbd6127bdd8e3efdee28fb
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2022-07-07 19:44:30 +0100

    Betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,14 @@
  */
 void print_line(int n)
 {
-    int i;
+int i;
 
-    for (i = 0; i< n; i++)
+for (i = 0; i< n; i++)
+{
+    if (n <= 0)
     {
-        if (n <= 0){
-            _putchar('\n');
-        }
-        _putchar('_');
-    }
+          _putchar('\n');
+     }
+      _putchar('_');
     _putchar('\n');
 }
