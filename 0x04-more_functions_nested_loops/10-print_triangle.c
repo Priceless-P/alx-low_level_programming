626c54631732f415f1a51ee850f6bec33d2bd7a0
Priceless-P <prisca.priceless11@gmail.com>
2022-07-08 02:07:32 +0100

    Betty

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,13 +14,13 @@ int n = size - 1;
 if (size > 0)
 {
 for (a = 0; a < size; a++)
-{ 
+{
 for (b = 0; b < size; b++)
 {
-  if(b < n)
-      _putchar(' ');
-  else
-      _putchar('#');
+if (b < n)
+_putchar(' ');
+else
+_putchar('#');
 }
 n--;
 _putchar('\n');
