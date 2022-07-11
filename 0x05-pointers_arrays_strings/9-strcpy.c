dec52be4fb57686fcc5caec0bda9bcb64eb44bed
Priceless-P <prisca.priceless11@gmail.com>
2022-07-11 22:37:37 +0100

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-    char *tmp = dest;
-
-    while (*src)
-        *dest++ = *src++;
-    return (tmp);
+int inc = 0;
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\0';
+return (dest);
 }
