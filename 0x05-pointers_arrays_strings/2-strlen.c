8160869584d2a27aba5246bb83c2251a7544567c
Priceless-P <prisca.priceless11@gmail.com>
2022-07-11 11:52:50 +0100

    Betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 /**
- * _strlen -> takes a pointer to an int as parameter and updates the value
+ * _strlen -> Check Description
+ * Description: takes a pointer to an int as parameter and updates the value
  * @s: value to be updated
  */
 
