7a9a3903924c2b10c5ce398f8d5f9c6631bd8211
Priceless-P <prisca.priceless11@gmail.com>
2022-07-11 08:46:34 +0100

    Task 0 to 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -> takes a pointer to an int as parameter and updates the value
+ * @a: value to be swapped with b
+ * @b: value to be swapped with a
+ * Return: Always 0.
+ */
+
+ void swap_int(int *a, int *b)
+ {
+ 	int z;
+
+ 	z = a;
+ 	*a = z;
+ 	*b = *a;
+ }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reset_to_98 -> takes a pointer to an int as parameter and updates the value
+ * @n: value to be updated
+ * Return: Always 0.
+ */
+
+ int _strlen(char *s)
+ {
+ 	int a = _strlen(s);
+
+ 	printf("%d", a);
+ }
