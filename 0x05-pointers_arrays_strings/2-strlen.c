c162d7162608a88ed32d9a7cbcda8641ab8b4dca
Priceless-P <prisca.priceless11@gmail.com>
2022-07-11 11:55:08 +0100

    Add return value

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 /**
- * _strlen -> Check Description
- * Description: takes a pointer to an int as parameter and updates the value
+ * _strlen -> takes a pointer to an int as parameter and updates the value
  * @s: value to be updated
+ * Return: 0 on success
  */
 
 int _strlen(char *s)
@@ -12,4 +12,6 @@ int _strlen(char *s)
 int a = _strlen(s);
 
 printf("%d", a);
+
+return (0);
 }
