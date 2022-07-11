cd374fa137afdb5ec8b53c3eb2121bbcc55f3046
Priceless-P <prisca.priceless11@gmail.com>
2022-07-11 11:51:06 +0100

    Hot fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,8 @@
 #include <string.h>
 
 /**
- * reset_to_98 -> takes a pointer to an int as parameter and updates the value
- * @n: value to be updated
- * Return: Always 0.
+ * _strlen -> takes a pointer to an int as parameter and updates the value
+ * @s: value to be updated
  */
 
 int _strlen(char *s)
