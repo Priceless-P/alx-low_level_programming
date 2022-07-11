a3dbdaba0ea1a9419a573e0e93446192c50ed784
Priceless-P <prisca.priceless11@gmail.com>
2022-07-11 21:50:34 +0100

    Hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,17 +8,13 @@
 
 void print_array(int *a, int n)
 {
-int i;
+int i = 0;
 
-for (i = 0; i < n; i++)
+for (; i < n; i++)
 {
 printf("%d", a[i]);
 if (i < n - 1)
 printf(", ");
-}
-else
-{
-	printf(" ")
 }
 _putchar('\n');
 }
