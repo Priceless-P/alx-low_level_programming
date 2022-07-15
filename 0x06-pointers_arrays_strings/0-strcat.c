37ee564ab8e02957a89e32baf474a01b743556df
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2022-07-15 08:22:57 +0100

    Betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,10 +16,12 @@ for (i = 0; dest[i] != '\0'; i++)
 ;
 
 do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
+a++;
+dest[i] = src[a];
+i++;
+}
+while (src[a] != '\0');
 
 return (dest);
 }
+
