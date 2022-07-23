e029ec15d5d451e4cb9e0ea8d90e9a550b988cfc
Priceless-P <prisca.priceless11@gmail.com>
2022-07-23 02:53:39 +0100

    Add other tasks and fix errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * @main - prints its name, followed by a new line. 
- * 
- * @argc: The number of arguments present in the program
- * @argv: An array of pointer to the arguments
- * Return: int 0 on success
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
  */
-int main (int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return (0);
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
