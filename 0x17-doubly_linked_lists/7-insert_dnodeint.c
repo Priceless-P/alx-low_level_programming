b8c72f6d5b48d02ccf435c464d7d8523b431c76b
Priceless-P <prisca.priceless11@gmail.com>
2022-09-29 17:21:28 +0100

    Correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,30 +10,35 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	unsigned int i = 0;
+	dlistint_t *node_prev = *h, *new;
 
+	if (*h == NULL)
+		return (NULL);
+	/* add node in the front */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
+	/* add node in the index */
+	while (node_prev != NULL)
 	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
+		if (i == idx - 1 && node_prev->next != NULL)
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = node_prev->next;
+			new->prev = node_prev;
+			node_prev->next = new;
+			new->next->prev = new;
+			return (new);
+		}
+		node_prev = node_prev->next;
+		i++;
 	}
 
-	if (tmp->next == NULL)
+	if (i == idx)
 		return (add_dnodeint_end(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
-
-	return (new);
+	return (NULL);
 }
