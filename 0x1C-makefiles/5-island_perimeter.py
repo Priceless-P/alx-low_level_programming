504cb1bfe622dfc3cb5298d7b60648e125d9f17c
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-11-22 21:35:30 +0100

    corrections

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,18 +2,23 @@
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid
+     The grid represents water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
     """
     rows = len(grid)
     cols = len(grid[0]) if rows > 0 else 0
 
-    perimeter = 0
+    ed = 0
+    sz = 0
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4
+                sz += 1
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
+                    ed += 1
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-    return perimeter
+                    ed += 1
+    return sz * 4 - ed * 2
